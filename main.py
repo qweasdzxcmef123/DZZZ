a071078beaf36d876a6d33ba6a0cac035af7f482
qweasdzxcmef123 <pashkov_07@list.ru>
2023-09-27 21:44:16 +0300

    123

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ c1 = int(input())
 a2 = int(input())
 b2 = int(input())
 c2 = int(input())
-cord1 = max(a1, a2) - min(a1, a2)
-cord2 = max(b1, b2) - min(b1, b2)
-cord3 = max(c1, c2) - min(c1, c2)
-cord4 = (cord1 ** 2+cord2 ** 2 + cord3**2)**0.5
-print((cord4**2) **0.5)
+cord1 = max(a1, a2)**2 - min(a1, a2)**2
+cord2 = max(b1, b2)**2 - min(b1, b2)**2
+cord3 = max(c1, c2)**2 - min(c1, c2)**2
+cord4 = (cord1 + cord2 + cord3)**0.5
+print(cord4)
